45599058cda3b160c2c9090159e9bb5ea21bf06b
angelalondonos <l.angela05@gmail.com>
2022-10-28 21:34:06 -0500

    Empty shopping cart

--- js/app.js
+++ js/app.js
@@ -13,6 +13,12 @@ function cargarEventListeners(){
     //Elimina cursos del carrito
     carrito.addEventListener('click', eliminarCurso);
 
+    //Vaciar el carrito
+    vaciarCarritoBtn.addEventListener('click', () =>{
+        articulosCarrito = []
+        limpiarHTML()
+    });
+
 }
 
 //Funciones
